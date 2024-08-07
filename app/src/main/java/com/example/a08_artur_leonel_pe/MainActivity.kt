a65e1eb069d7f4dd0dd11dcfaf8424832c7ee018
Artur <Bolonha@hotmail.com>
2024-08-07 17:46:58 -0300

    Telas
    Criado todas as telas que serão utilizadas

--- app/src/main/java/com/example/a08_artur_leonel_pe/MainActivity.kt
+++ app/src/main/java/com/example/a08_artur_leonel_pe/MainActivity.kt
@@ -14,6 +14,7 @@ import com.google.android.material.internal.NavigationMenu
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    //declarando o drawerlayout
     private lateinit var drawerLayout: DrawerLayout
 
     @SuppressLint("MissingInflatedId")
@@ -22,20 +23,33 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         enableEdgeToEdge()
         setContentView(R.layout.base)
 
+        //declarando os widgets/views
         drawerLayout = findViewById(R.id.drawer_layout)
         var navView = findViewById<NavigationView>(R.id.navView)
         navView.setNavigationItemSelectedListener (this)
 
+        //Inflar o activity main no frame_content do layout base
         layoutInflater.inflate(R.layout.activity_main, findViewById(R.id.frame_content))
 
+        //Botão do menu caso seja acionado, ele abre o menu
         val menu = findViewById<ImageView>(R.id.menu).setOnClickListener{
             drawerLayout.openDrawer(GravityCompat.START)
         }
     }
 
+    //Menu Em Ação
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
+            R.id.nav_Home -> {
+
+            }
             R.id.nav_contato -> {
+
+            }
+            R.id.nav_Configurações -> {
+
+            }
+            R.id.nav_WorldSkills -> {
 
             }
         }
